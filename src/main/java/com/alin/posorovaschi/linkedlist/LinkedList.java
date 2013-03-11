c287e7dbcddf0775cf41e4ea6529d62ec95f76ea
alinposho <alinposho@yahoo,com>
2013-03-11 20:55:41 +0200

    Add the implementation for the LinkedList node element.

---
+++ src/main/java/com/alin/posorovaschi/linkedlist/LinkedList.java
@@ -0,0 +1,9 @@
+package com.alin.posorovaschi.linkedlist;
+
+/**
+ * This is the container for the Linked List that implements reversing operations
+ */
+public class LinkedList {
+
+
+}
---
+++ src/main/java/com/alin/posorovaschi/linkedlist/Node.java
@@ -0,0 +1,29 @@
+package com.alin.posorovaschi.linkedlist;
+
+/**
+ * This is the Linked List element - a node in the list.
+ */
+public class Node<T> {
+
+    private final T value;
+
+    private Node next;
+
+    public Node(T value, Node next) {
+        this.value = value;
+        this.next = next;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+}
