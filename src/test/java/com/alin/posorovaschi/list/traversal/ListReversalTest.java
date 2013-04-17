81d65d634a0373c8ad5da9972e452b1f440758fe
alinposho <alinposho@yahoo,com>
2013-04-17 21:25:07 +0300

    Add a dupliate assert.

--- src/test/java/com/alin/posorovaschi/list/traversal/ListReversalTest.java
+++ src/test/java/com/alin/posorovaschi/list/traversal/ListReversalTest.java
@@ -75,6 +75,7 @@ public abstract class ListReversalTest {
 
         // Verify
         assertEquals(list, resultList);
+        assertEquals(list, resultList);
         assertEquals(secondElementValue, list.getHead().getValue());
         assertEquals(firstElementValue, list.getHead().getNext().getValue());
 
